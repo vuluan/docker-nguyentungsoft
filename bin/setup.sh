819bb998f2cdfd8abeffae5d8de955073d62694c
Luan Nguyen <l.nguyen@shopmacher.de>
2018-01-22 14:29:51 +0700

    update

--- bin/setup.sh
+++ bin/setup.sh
@@ -7,7 +7,7 @@ echo "-- Running script with「BASE_DIR」is: ${BASE_DIR} --";
 cd ${BASE_DIR};
 
 # Docker up
-docker-compose up --build -d;
+COMPOSE_HTTP_TIMEOUT=200 docker-compose up --build -d;
 
 # Showing information
 docker-compose ps
--- web/docker-entrypoint.sh
+++ web/docker-entrypoint.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-sudo a2enmod rewrite;
+a2enmod rewrite;
+apt-get install php7.0-xml;
 service apache2 start;
 tail -f /dev/null;
